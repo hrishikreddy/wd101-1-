5d6ba12d5027c0a1565b217917a1c5491cb3716d
hrishikreddy <147172349+hrishikreddy@users.noreply.github.com>
2023-11-21 18:19:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,65 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const userTableBody = document.getElementById('userTableBody');
+  const userTableBody = document.querySelector('#userTableBody');
 
-    for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        if (key.startsWith('user_')) {
-            const userData = JSON.parse(localStorage.getItem(key));
-            addRowToTable(userTableBody, userData);
-        }
+  const registeredUsers = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (key.startsWith('user_')) {
+      const userData = JSON.parse(localStorage.getItem(key));
+      registeredUsers.push(userData);
+      addRowToTable(userTableBody, userData);
     }
-});
+  }
 
-const form = document.getElementById('registrationForm');
-form.addEventListener('submit', (event) => {
+  const registrationForm = document.getElementById('registrationForm');
+  registrationForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const dob = new Date(form.dob.value);
+    const dob = new Date(registrationForm.dob.value);
     const currentYear = new Date().getFullYear();
     const age = currentYear - dob.getFullYear();
 
     // Validate age to accept users between 18 and 55 years old
     if (age < 18 || age > 55) {
-        alert('Age should be between 18 and 55.');
-        return;
+      alert('Age should be between 18 and 55.');
+      return;
     }
 
-    const userKey = 'user_' + Date.now();
-    localStorage.setItem(userKey, JSON.stringify({
-        name: form.name.value,
-        email: form.email.value,
-        password: form.password.value,
-        dob: form.dob.value,
-        acceptedTerms: form.acceptedTerms.checked
-    }));
+    const user = {
+      name: registrationForm.name.value,
+      email: registrationForm.email.value,
+      password: registrationForm.password.value,
+      dob: registrationForm.dob.value,
+      acceptedTerms: registrationForm.acceptedTerms.checked
+    };
 
-    const userTableBody = document.getElementById('userTableBody');
-    addRowToTable(userTableBody, {
-        name: form.name.value,
-        email: form.email.value,
-        password: form.password.value,
-        dob: form.dob.value,
-        acceptedTerms: form.acceptedTerms.checked
-    });
+    registeredUsers.push(user);
+    localStorage.setItem(`user_${user.name}`, JSON.stringify(user));
+
+    addRowToTable(userTableBody, user);
+  });
 });
 
 function addRowToTable(tableBody, userData) {
-    const newRow = tableBody.insertRow();
-    
-    // Add borders and padding to each cell
-    const cellStyle = 'border border-gray-300 p-2';
+  const newRow = tableBody.insertRow();
 
-    const nameCell = newRow.insertCell();
-    nameCell.textContent = userData.name;
-    nameCell.className = cellStyle;
+  const nameCell = newRow.insertCell();
+  nameCell.textContent = userData.name;
+  nameCell.classList.add('border', 'border-gray-300', 'p-2');
 
-    const emailCell = newRow.insertCell();
-    emailCell.textContent = userData.email;
-    emailCell.className = cellStyle;
+  const emailCell = newRow.insertCell();
+  emailCell.textContent = userData.email;
+  emailCell.classList.add('border', 'border-gray-300', 'p-2');
 
-    const passwordCell = newRow.insertCell();
-    passwordCell.textContent = userData.password;
-    passwordCell.className = cellStyle;
+  const passwordCell = newRow.insertCell();
+  passwordCell.textContent = userData.password;
+  passwordCell.classList.add('border', 'border-gray-300', 'p-2');
 
-    const dobCell = newRow.insertCell();
-    dobCell.textContent = userData.dob;
-    dobCell.className = cellStyle;
+  const dobCell = newRow.insertCell();
+  dobCell.textContent = userData.dob;
+  dobCell.classList.add('border', 'border-gray-300', 'p-2');
 
-    const acceptedTermsCell = newRow.insertCell();
-    acceptedTermsCell.textContent = userData.acceptedTerms;
-    acceptedTermsCell.className = cellStyle;
-      }
+  const acceptedTermsCell = newRow.insertCell();
+  acceptedTermsCell.textContent = userData.acceptedTerms;
+  acceptedTermsCell.classList.add('border', 'border-gray-300', 'p-2');
+}
